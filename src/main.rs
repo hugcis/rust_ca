6083163ceb0f62cf1a373a1d31ad87a056073d5c
hugcis <hmj.cisneros@gmail.com>
2021-01-28 14:00:52 +0100

    Remove copy-pasted doc and unify the interface for writing output

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
 #![feature(test)]
 #![cfg_attr(test, deny(warnings))]
 #![deny(missing_docs)]
-//! The main crate for Iron.
-//!
-//! ## Overview
-//!
-//! Iron is a high level web framework built in and for Rust, built on
-//! [hyper](https://github.com/hyperium/hyper). Iron is designed to take advantage
-//! of Rust's greatest features - its excellent type system and principled
-//! approach to ownership in both single threaded and multi threaded contexts.
-//!
-//! Iron is highly concurrent and can scale horizontally on more machines behind a
-//! load balancer or by running more threads on a more powerful machine. Iron
-//! avoids the bottlenecks encountered in highly concurrent code by avoiding shared
-//! writes and locking in the core framework.
+//! The main crate for CellularAutomata-rs.
 
 // Third-party packages
 extern crate test;
 
-use crate::automaton::{Automaton, TiledAutomaton, TILE_SIZE};
-use crate::rule::Rule;
-use std::env;
-
 mod automaton;
 mod output;
 mod rule;
+
+use crate::automaton::{Automaton, TiledAutomaton, TILE_SIZE};
+use crate::rule::Rule;
+use std::env;
 
 struct SimulationOpts {
     size: u16,
@@ -122,20 +110,9 @@ fn main() {
     if opts.size as usize % (TILE_SIZE - 1) == 0 {
         let mut a = TiledAutomaton::new(opts.states, opts.size.into(), opts.rule);
         a.random_init();
-        output::write_to_gif_file_tiled(
-            Some("test.gif"),
-            &mut a,
-            opts.scale,
-            opts.steps,
-            opts.skip,
-        );
+        output::write_to_gif_file(Some("test.gif"), &mut a, opts.scale, opts.steps, opts.skip);
     } else {
-        let mut a = Automaton::new(
-            opts.states,
-            opts.size.into(),
-            vec![0; opts.size as usize * opts.size as usize],
-            opts.rule,
-        );
+        let mut a = Automaton::new(opts.states, opts.size.into(), opts.rule);
         a.random_init();
         output::write_to_gif_file(Some("test.gif"), &mut a, opts.scale, opts.steps, opts.skip);
     };
@@ -152,7 +129,7 @@ mod tests {
     fn write_autom(b: &mut Bencher) {
         let r = Rule::random(1, 3);
         let size: u16 = 512;
-        let mut a = Automaton::new(3, size.into(), vec![0; size as usize * size as usize], r);
+        let mut a = Automaton::new(3, size.into(), r);
         a.random_init();
         b.iter(|| output::write_to_gif_file(Some("test.gif"), &mut a, 1, 10, 1))
     }
@@ -162,6 +139,6 @@ mod tests {
         let size: u16 = 512;
         let mut a = TiledAutomaton::new(3, size.into(), r);
         a.random_init();
-        b.iter(|| output::write_to_gif_file_tiled(Some("test.gif"), &mut a, 1, 10, 1))
+        b.iter(|| output::write_to_gif_file(Some("test.gif"), &mut a, 1, 10, 1))
     }
 }
--- src/output.rs
+++ src/output.rs
@@ -1,76 +1,32 @@
-use crate::automaton::{TiledGrid, TILE_SIZE};
-use crate::{Automaton, TiledAutomaton};
+use crate::automaton::AutomatonImpl;
 use gif::{Encoder, Frame};
 use std::fs::File;
 
-pub fn write_to_gif_file(
-    fname: Option<&str>,
-    autom: &mut Automaton,
-    scale: u16,
-    steps: u32,
-    skip: u32,
-) {
-    let size = autom.size as u16;
+pub fn write_to_gif_file<T>(fname: Option<&str>, autom: &mut T, scale: u16, steps: u32, skip: u32)
+where
+    T: AutomatonImpl,
+{
+    let size = autom.get_size() as u16;
     let scaled_size = size * scale;
-    let states = autom.states;
+    let states = autom.get_states();
 
     let mut im_file = File::create(fname.unwrap_or("test.gif")).unwrap();
     let mut g = Encoder::new(&mut im_file, scaled_size, scaled_size, &[]).unwrap();
     g.set_repeat(gif::Repeat::Infinite).unwrap();
 
-    let u = autom.skipped_iter(steps, skip);
+    let autom_iterator = autom.skipped_iter(steps, skip, scale);
     let mut c = 0;
     let palette = make_palette(states);
-    let frames = u.map(|s| {
-        let inf = Frame::from_palette_pixels(
-            scaled_size,
-            scaled_size,
-            &duplicate_array(s, size as usize, scale),
-            &palette,
-            None,
-        );
-        println!("Processing image {}", c);
+    let frames = autom_iterator.map(|grid| {
+        let frame = Frame::from_palette_pixels(scaled_size, scaled_size, &grid, &palette, None);
+        print!("\rProcessing image {}/{}", c + 1, steps);
         c += 1;
-        inf
+        frame
     });
     for frame in frames {
         g.write_frame(&frame).expect("Error writing frame");
     }
-}
-
-pub fn write_to_gif_file_tiled(
-    fname: Option<&str>,
-    autom: &mut TiledAutomaton,
-    scale: u16,
-    steps: u32,
-    skip: u32,
-) {
-    let size = autom.size as u16;
-    let scaled_size = size * scale;
-    let states = autom.states;
-
-    let mut im_file = File::create(fname.unwrap_or("test.gif")).unwrap();
-    let mut g = Encoder::new(&mut im_file, scaled_size, scaled_size, &[]).unwrap();
-    g.set_repeat(gif::Repeat::Infinite).unwrap();
-
-    let u = autom.skipped_iter(steps, skip);
-    let mut c = 0;
-    let palette = make_palette(states);
-    let frames = u.map(|s| {
-        let inf = Frame::from_palette_pixels(
-            scaled_size,
-            scaled_size,
-            &duplicate_array_tiled(s, size as usize, scale),
-            &palette,
-            None,
-        );
-        println!("Processing image {}", c);
-        c += 1;
-        inf
-    });
-    for frame in frames {
-        g.write_frame(&frame).expect("Error writing frame");
-    }
+    print!("\n");
 }
 
 fn make_palette(states: u8) -> Vec<u8> {
@@ -85,37 +41,4 @@ fn make_palette(states: u8) -> Vec<u8> {
         palette.push((col_1[2] * t + col_2[2] * (1. - t)) as u8);
     }
     palette
-}
-
-#[inline]
-fn duplicate_array(s: Vec<u8>, size: usize, scale: u16) -> Vec<u8> {
-    let scaled_size = size * scale as usize;
-    let mut out = Vec::with_capacity(scaled_size * scaled_size);
-    for i in 0..scaled_size {
-        for j in 0..scaled_size {
-            let item = s[(i / scale as usize) * size + (j / scale as usize)];
-            out.push(item);
-        }
-    }
-    out
-}
-
-#[inline]
-fn duplicate_array_tiled(s: TiledGrid, size: usize, scale: u16) -> Vec<u8> {
-    let scaled_size = size * scale as usize;
-    let n_tiles = size / TILE_SIZE;
-    let mut out = Vec::with_capacity(scaled_size * scaled_size);
-    for a in 0..scaled_size {
-        for b in 0..scaled_size {
-            let i = a / scale as usize;
-            let j = b / scale as usize;
-            let tx = i / (TILE_SIZE - 1);
-            let ty = j / (TILE_SIZE - 1);
-            let x = i % (TILE_SIZE - 1);
-            let y = j % (TILE_SIZE - 1);
-            let item = s[tx * n_tiles + ty][x * TILE_SIZE + y];
-            out.push(item);
-        }
-    }
-    out
 }
